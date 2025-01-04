63a61f6812a0fdce5886e84acf49da59cb3c3bb5
thijsvdc <thijsvdc@gmail.com>
2025-01-04 14:37:20 +0100

    Update config_flow.py

--- custom_components/metar_plugin/config_flow.py
+++ custom_components/metar_plugin/config_flow.py
@@ -1,9 +1,8 @@
 from homeassistant import config_entries
-from homeassistant.core import callback
+import voluptuous as vol
 
 from .const import DOMAIN
 
-@config_entries.HANDLERS.register(DOMAIN)
 class MetarPluginConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for METAR Plugin."""
 
@@ -13,10 +12,11 @@ class MetarPluginConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title="METAR Plugin", data=user_input)
+            return self.async_create_entry(title=f"METAR: {user_input['icao']}", data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({vol.Required("icao"): str}),
+            data_schema=vol.Schema({
+                vol.Required("icao", default="KJFK"): str,  # Default ICAO code
+            }),
         )
-
