8582f33a36493b0c14984a4f9e8c0ab53593b0bd
thijsvdc <thijsvdc@gmail.com>
2025-01-04 14:29:36 +0100

    Update __init__.py

--- custom_components/__init__.py
+++ custom_components/__init__.py
@@ -1,1 +1,6 @@
-
+async def async_setup_entry(hass, entry):
+    """Set up METAR Plugin from a config entry."""
+    icao = entry.data["icao"]
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = icao
+    return True
