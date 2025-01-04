a90fe426da44dd5951901784a8bfefa36de8ac65
thijsvdc <thijsvdc@gmail.com>
2025-01-04 23:49:45 +0100

    Update __init__.py

--- custom_components/metar_plugin/__init__.py
+++ custom_components/metar_plugin/__init__.py
@@ -1,32 +1,16 @@
-from homeassistant.core import HomeAssistant
+"""The METAR integration."""
+from __future__ import annotations
+
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import ConfigType
-from .const import DOMAIN
-import logging
+from homeassistant.core import HomeAssistant
 
-LOGGER = logging.getLogger(__name__)
-
-LOGGER.info("METAR Plugin: Starting setup...")
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the METAR Plugin integration."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
+from .const import DOMAIN, PLATFORMS
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up METAR Plugin from a config entry."""
-    icao = entry.data["icao"]
-
-    # Store the configuration in domain data
-    hass.data[DOMAIN][entry.entry_id] = icao
-
-    # Set up the sensor platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    """Set up METAR from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
