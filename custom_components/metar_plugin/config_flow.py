05de476e738d9b615028502c3d6bc0bf1f0e1bfc
thijsvdc <thijsvdc@gmail.com>
2025-01-04 14:45:47 +0100

    Update config_flow.py

--- custom_components/metar_plugin/config_flow.py
+++ custom_components/metar_plugin/config_flow.py
@@ -7,16 +7,18 @@ class MetarPluginConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for METAR Plugin."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title=f"METAR: {user_input['icao']}", data=user_input)
+            return self.async_create_entry(
+                title=f"METAR: {user_input['icao']}",
+                data=user_input
+            )
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("icao", default="EBBR"): str,  # Default ICAO code
-            }),
+                vol.Required("icao", default="EBBR"): str
+            })
         )
