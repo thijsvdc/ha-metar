8fe4d4ffbf6e7336ed3477688d9aabe7ebce4c35
thijsvdc <thijsvdc@gmail.com>
2025-01-04 23:50:26 +0100

    Update const.py

--- custom_components/metar_plugin/const.py
+++ custom_components/metar_plugin/const.py
@@ -1,1 +1,19 @@
-DOMAIN = "metar_plugin"
+"""Constants for the METAR integration."""
+from datetime import timedelta
+
+DOMAIN = "metar"
+PLATFORMS = ["sensor"]
+
+CONF_STATION = "station"
+DEFAULT_NAME = "METAR"
+DEFAULT_UPDATE_INTERVAL = timedelta(minutes=30)
+
+ATTR_STATION = "station"
+ATTR_LAST_UPDATE = "last_update"
+ATTR_TEMPERATURE = "temperature"
+ATTR_DEWPOINT = "dewpoint"
+ATTR_WIND = "wind"
+ATTR_VISIBILITY = "visibility"
+ATTR_PRESSURE = "pressure"
+ATTR_WEATHER = "weather"
+ATTR_CLOUDS = "clouds"
